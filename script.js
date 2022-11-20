1d58515164307f44cd3e099a467d0af7f6f56178
jaehoon <ljh1032112@gmail.com>
2022-11-20 19:52:39 +0900

    Update login

--- script.js
+++ script.js
@@ -636,15 +636,24 @@ window.onload = function () {
 
 window.onload = function () {
    const btn = document.querySelector('#btn');
-
    btn.addEventListener('click', function () {
-       let n_v = document.getElementById('name').value;
-       let p_v = document.getElementById('password').value;
-       if (n_v == '1032' && p_v == '1032') {
-           var link = '/lotto/index.html';
-           location.href=link;
-       } else {
-           
-       }
+      login()
    });
+}
+
+function enterkey() {
+   if (window.event.keyCode == 13) {
+      login()
+   }
+}
+
+function login() {
+   let n_v = document.getElementById('name').value;
+   let p_v = document.getElementById('password').value;
+   if (n_v == '1032' && p_v == '1032') {
+      var link = '/lotto/index.html';
+      location.href = link;
+   } else {
+      alert("ID and Password do not match...")
+   }
 }
